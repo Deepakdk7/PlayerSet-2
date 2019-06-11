9628d7e923a01b99c2e934a14b978ff997c67272
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-11 21:29:02 +0530

    Update 14.py

--- 14.py
+++ 14.py
@@ -1,8 +1,8 @@
-sx=int(input())
-ax=list(input())
-for i in ax:
+dx=int(input())
+x=list(input())
+for i in x:
     if (i=='a' or i=='e' or i=='i' or i=='o' or i=='u'):
-        ax.remove(i)
-ax=ax[::-1]
-for i in ax:
+        x.remove(i)
+x=x[::-1]
+for i in x:
     print(i,end="")
