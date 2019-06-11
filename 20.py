cb3d63275c36defcd6f8d63cce8142c2f2911240
Deepakdk7 <51220607+Deepakdk7@users.noreply.github.com>
2019-06-11 22:33:02 +0530

    Update 20.py

--- 20.py
+++ 20.py
@@ -2,9 +2,9 @@ ax=list(input())
 c=[]
 a=0
 for i in ax:
-    if ord(i)>26:
-        a=ord(i)-26
-        c.append(a+3)
+    if ord(i)+3>90:
+        a=ord(i)+3
+        c.append(a-26)
     else:
         c.append(ord(i)+3)
 for i in c:
